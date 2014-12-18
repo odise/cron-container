3e1b0c91a1a8173160d4b5fa5c8fc92b9eb24f33
Jan Nabbefeld <jan.nabbefeld@kreuzwerker.de>
2014-12-18 19:01:56 +0100

    Envplate bug workaround

--- etcdbackupjob.sh
+++ etcdbackupjob.sh
@@ -1,3 +1,5 @@
+DUMPFILE=${ETCD_DUMP_FILENAME:-dump.json}
+
 cat << EOF > /root/etcd-configuration.json
 {
   "cluster": {
@@ -20,7 +22,7 @@ cat << EOF > /root/backup-configuration.json
 {
   "concurrentRequests": 50,
   "retries": 5,
-  "dumpFilePath": "/root/${ETCD_DUMP_FILENAME:-dump.json}",
+  "dumpFilePath": "/root/$DUMPFILE",
   "backupStrategy": {
     "keys": ["${ETCD_KEY:-/_coreos.com}"],
     "sorted": false,
@@ -31,4 +33,5 @@ EOF
 
 /root/etcd-backup -config=/root/backup-configuration.json \
   -etcd-config=/root/etcd-configuration.json dump && \
-  /root/gof3r cp --endpoint=${AWS_S3_ENDPOINT:-s3-eu-west-1.amazonaws.com} --debug /root/${ETCD_DUMP_FILENAME:-dump.json} s3://${AWS_S3_BUCKET}/${ETCD_DUMP_FILENAME:-dump.json}
+  /root/gof3r cp --endpoint=${AWS_S3_ENDPOINT:-s3-eu-west-1.amazonaws.com} \
+  --debug /root/$DUMPFILE s3://${AWS_S3_BUCKET}/$DUMPFILE
